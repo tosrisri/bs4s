159a4111a11247930a4f5360975d62b625a59ff8
Srini <tosrisri@gmail.com>
2019-03-13 18:51:16 +0530

    added index.html, main.css, main.js, wave2.svg

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Typed script for intro
+var options = {
+	strings: ["Srinivas Buddha.","UI Developer.", "UX &amp; UI Designer.", "Photographer."],
+	typeSpeed: 60,
+	loop: true,
+	backSpeed: 20,
+	backDelay: 1500
+  }
+var typed = new Typed(".typed", options);
+
